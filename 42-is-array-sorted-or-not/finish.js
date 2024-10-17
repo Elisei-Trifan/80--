02a484ac82a0eff31b213563f774dd4b33360b48
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-17 17:43:36 +0300

    array sorted or not

--- 42-is-array-sorted-or-not/finish.js
+++ 42-is-array-sorted-or-not/finish.js
@@ -16,6 +16,27 @@ const b = [4, 10, 14, 25, 25, 50]
 const c = [150, 132, 80, 40]
 const d = [15, 26, 10, 23, 85]
 
+function arraySortInfo(inputArray) {
+  if (inputArray.some((item) => typeof item !== 'number')) {
+    return 'Некоторые элементы не являются числами'
+  }
+  if (
+    inputArray.every((item, index) =>
+      index > 0 ? item >= inputArray[index - 1] : true
+    )
+  ) {
+    return 'Массив отсортирован по возрастанию'
+  }
+  if (
+    inputArray.every((item, index) =>
+      index > 0 ? item <= inputArray[index - 1] : true
+    )
+  ) {
+    return 'Массив отсортирован по убыванию'
+  }
+  return 'Массив не отсортирован'
+}
+
 console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
 console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
 console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
