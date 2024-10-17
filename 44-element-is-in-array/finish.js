6fa8c4df86cf348bb93166ae0fc630b7b98234d5
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-17 18:27:48 +0300

    searching element in array

--- 44-element-is-in-array/finish.js
+++ 44-element-is-in-array/finish.js
@@ -8,6 +8,9 @@
  */
 
 const transports = ['Bus', 'Car', 'Bicycle', 'Airplane']
+function isElementInArray(inputArray, searchElement) {
+  return inputArray.includes(searchElement)
+}
 
 console.log(isElementInArray(transports, 'Bus')) // true
 console.log(isElementInArray(transports, 'Phone')) // false
