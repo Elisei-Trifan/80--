e8c432c92ba104e20fd76430a2d9664c87e981dd
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-19 13:03:40 +0300

    JSON.stringify

--- 45-includes-object-or-array/finish.js
+++ 45-includes-object-or-array/finish.js
@@ -23,6 +23,15 @@ const fruits = [
 
 const primitiveTypesArray = [25, 'x', true, undefined, null]
 
+function isElementInArray(searchElement, inputArray) {
+  if (typeof searchElement !== 'object') {
+    return inputArray.includes(searchElement)
+  }
+  return inputArray
+    .map((item) => JSON.stringify(item))
+    .includes(JSON.stringify(searchElement))
+}
+
 console.log(isElementInArray(['css', 'flexbox'], tags)) // true
 
 console.log(isElementInArray(['flexbox', 'css'], tags)) // false
