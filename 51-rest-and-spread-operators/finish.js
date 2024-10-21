14afb1219753fc707f99a6b2c28db1018fca7343
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-21 15:56:15 +0300

    some()

--- 51-rest-and-spread-operators/finish.js
+++ 51-rest-and-spread-operators/finish.js
@@ -12,14 +12,16 @@
  * ПОДСКАЗКА: В этом задании вы должны использовать как оператор "rest",
  * так и оператор "spread".
  */
-function meanScore(a) {
-  return a.reduce((acc, item) => acc + item / a.length, 0).toFixed(2)
+function meanScore(...a) {
+  return a.some((item) => typeof item !== 'number')
+    ? 'Все аргументы в вызове функции должны быть числами!'
+    : a.reduce((acc, item) => acc + item / a.length, 0).toFixed(2)
 }
 const scores1 = [0, 1.5, 2.5, 3.7]
 const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2]
 const scores3 = [1.3, 2.5, 1.9]
 const scores4 = ['abc', 1.3, true, 2.5, 1.9]
-console.log(meanScore(scores1))
+console.log(meanScore(...scores4))
 // console.log(meanScore(/* все элементы из массива "scores1" */))
 // // 1.93
 
