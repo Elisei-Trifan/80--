8384c1baec3ab52d99efccc00e207f83aca1d3a4
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-22 13:25:56 +0300

    scope in function

--- 63-function-scope/finish.js
+++ 63-function-scope/finish.js
@@ -8,16 +8,15 @@ const b = 2
 let d = 15
 
 function myFn1(a) {
+  function myFn2(a) {
+    let c = 5
+    console.log(a, b, c, d)
+    // a - undefined, b - 2, c - 5, d - 15
+    // a - undefined, b - undefined, c - 5, d - 10
+  }
   let b
   let d = 10
   myFn2(b)
 }
 
-function myFn2(a) {
-  let c = 5
-  console.log(a, b, c, d)
-}
-
 myFn1()
-
-// a - undefined, b - 2, c - 5, d - 15
