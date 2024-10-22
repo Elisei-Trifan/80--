4f6c7b9faeed6ef7e552fbb920db89cf21ce7dab
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-22 13:11:04 +0300

    functions differens

--- 62-different-functions/finish.js
+++ 62-different-functions/finish.js
@@ -7,6 +7,13 @@
  * 3. Также вызовите обе функции
  */
 
+// // We can assign new value
+// firstFunction = '123'
+// console.log(firstFunction)
+
+// // Cannot access 'secondFunction' before initialization
+// secondFunction(2, 7)
+
 function firstFunction(a, b) {
   return a + b
 }
@@ -14,3 +21,6 @@ function firstFunction(a, b) {
 const secondFunction = function (a, b) {
   return a + b
 }
+
+console.log(firstFunction(1, 5))
+console.log(secondFunction(1, 5))
