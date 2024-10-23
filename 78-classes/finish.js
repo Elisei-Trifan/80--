dd7d2dafc23eb8541de97beecd36f5dc6e6739f7
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-23 11:24:00 +0300

    classes

--- 78-classes/finish.js
+++ 78-classes/finish.js
@@ -13,6 +13,16 @@
  * 4. Выводы в консоли должны совпасть
  */
 
+class Fruit {
+  constructor(title, price) {
+    this.title = title
+    this.price = price
+  }
+  priceInfo() {
+    return `Price of the ${this.title} is ${this.price}$`
+  }
+}
+
 const apple = new Fruit('Apple', 2)
 console.log(apple.priceInfo())
 // Price of the Apple is 2$
@@ -20,3 +30,6 @@ console.log(apple.priceInfo())
 const orange = new Fruit('Orange', 3)
 console.log(orange.priceInfo())
 // Price of the Orange is 3$
+
+const tomate = new Fruit('помидор', 10)
+console.log(tomate.priceInfo())
