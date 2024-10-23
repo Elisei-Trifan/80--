2007fb5ec063c8c955b54a959ef9950a9f92241f
Elisei-Trifan <slavaberzan@gmail.com>
2024-10-23 11:03:02 +0300

    closures

--- 77-function-closures/finish.js
+++ 77-function-closures/finish.js
@@ -14,6 +14,20 @@
  *  - changeGreeting
  */
 
+function createGreeting() {
+  let greetingString = 'Hey, this is'
+  function greet(name) {
+    return `${greetingString} ${name}`
+  }
+  function changeGreeting(str) {
+    greetingString = str
+  }
+  return {
+    greet,
+    changeGreeting,
+  }
+}
+
 const greeting1 = createGreeting()
 
 console.log(greeting1.greet('Bob'))
